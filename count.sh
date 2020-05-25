da674f19120c69eacbcb959643f537fd7b196023
Derek Roberts <derek.roberts@gmail.com>
2020-05-25 16:34:17 -0700

    Cut out keyword var and summary

--- count.sh
+++ count.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 #%
-#% HTTP Status Checker for CSV Files
-#%
 #%   Reads a CSV file of websites and outputs their statuses into results.csv
+#%
+#%     [TIMEOUT=10] ./count.sh ./input.csv [./results.csv]
 #%
 
 # Specify halt conditions (errors, unsets, non-zero pipes), field separator and verbosity
@@ -11,25 +11,13 @@ set -euo pipefail
 IFS=$'\n\t'
 [ ! "${VERBOSE:-}" == "true" ] || set -x
 
-# Input and output files
+# Variables - input, output and curl timeout
 #
-INPUT_CSV=${1}
-SAVE_OUT=${2:-./results.csv}
+INPUT_CSV="${1}"
+SAVE_OUT="${2:-./results.csv}"
+TIMEOUT="${TIMEOUT:-15}"
 
-# Config variables
-#
-TIMEOUT=5
-KEYWORDS="
-    200
-    400
-    403
-    404
-    503
-    Unavailable
-    Excluded
-"
-
-# Function - clean up input (exclude ftp, \\ and trim ,, /, #.*, ?.*, (.*)
+# Clean up input - exclude ftp \\, remove http[s]:// and clip after , / # ? (
 #
 url_cleaner() {
     echo ${1} | sed \
@@ -37,13 +25,13 @@ url_cleaner() {
         -e 's/^\\.*//g' \
         -e 's/^ht[t]p[s]\?:\/\///g' \
         -e 's/,*$//g' \
-        -e 's/ *(.*$//g' \
+        -e 's/(.*$//g' \
         -e 's/?.*$//g' \
         -e 's/#.*$//g' \
         -e 's/\/*$//g'
 }
 
-# Function - return HTTP code, Excluded or Unavailable, depending on parameter
+# Return HTTP status code, Excluded or Unavailable
 #
 curl_runner() {
     TO_CURL=$(url_cleaner ${1})
@@ -54,25 +42,14 @@ curl_runner() {
     esac
 }
 
-# Curl sites, store with results in CSV
+# Curl sites, echoing and saving results
 #
 echo >"${SAVE_OUT}"
 while read -r in; do
-    echo; echo "${in}"
-    RESULT=$(curl_runner ${in})
-    echo "${in%,}, ${RESULT}" >>"${SAVE_OUT}"
-    echo "${RESULT}"
+    echo "${in%,}, $(curl_runner ${in})" | tee -a "${SAVE_OUT}"
 done <"${INPUT_CSV}"
-
-# Tally results
-#
-echo -e "\n ---\nResults"
-for k in $(echo "${KEYWORDS}" | sed 's/^[ \t]*//g'); do
-    echo "  ${k}: $(grep "$k" $SAVE_OUT | wc -l)"
-done
 
 # Summarize
 #
-echo -e "\n ---\nSummary"
-echo "  Received: $(grep -cve '^\s*$' "${INPUT_CSV}")"
-echo "  Returned: $(grep -cve '^\s*$' "${SAVE_OUT}")"
+echo -e "\n---\nSummary"
+echo -e "  $(grep -cve '^\s*$' ${INPUT_CSV}) in / $(grep -cve '^\s*$' ${SAVE_OUT}) out \n"
