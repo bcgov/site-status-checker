802a94bfc6dfd63ba2b39118b98b9550b8740a5a
Derek Roberts <derek.roberts@gmail.com>
2020-06-03 10:16:48 -0700

    Revise help message, arrangement

--- count.sh
+++ count.sh
@@ -1,29 +1,36 @@
 #!/bin/sh -l
 #%
-#%   Reads a CSV file of websites and outputs their statuses into results.csv
+#%   Reads a CSV file of websites (header=URL) and outputs their statuses (header=HTTP_STATUS)
 #%
-#%     [TIMEOUT=10] THIS_FILE ./input.csv [./results.csv]
+#%     [TIMEOUT=10] THIS_FILE.sh ./input.csv [./results.csv]
 #%
 
 # Boilerplate - halt conditions (errors, unsets, non-zero pipes), verbosity and help (w/o params)
 #
 set -euo pipefail
 [ ! "${VERBOSE:-}" == "true" ] || set -x
-(($#)) || { grep "^#%" ${0} | sed -e "s/^#%//g" -e "s|THIS_FILE|${0}|g"; exit; }
+(($#)) || { grep "^#%" ${0} | sed -e "s/^#%//g" -e "s|THIS_FILE.sh|${0}|g"; exit; }
 
-# Variables - input, output and curl timeout
+# Variable defaults - can assign at runtime with VARIABLE=value
 #
-INPUT_CSV="${1}"
-SAVE_OUT="${2:-./results.csv}"
 TIMEOUT="${TIMEOUT:-15}"
 HEADER_IN="${HEADER_IN:-URL}"
 HEADER_OUT="${HEADER_OUT:-HTTP_STATUS}"
+
+# Variables - input, output files
+#
+INPUT_CSV="${1}"
+SAVE_OUT="${2:-./results.csv}"
+
+# Internal field separator is usually a comma for CSV files
+#
+IFS=','
 
 # Return first match in header or error and length+1 for appending
 #
 header_position() {
     i=0
-    head -1 "${INPUT_CSV}" | tr "," "\n" | while IFS=, read -r h; do
+    head -1 "${INPUT_CSV}" | tr "," "\n" | while read -r h; do
         ((i=i+1))
         [ "${h}" != "${1}" ] || echo "${i}"
     done
@@ -35,7 +42,7 @@ url_cleaner() {
     echo ${1} | sed \
         -e 's/^[(ftp:)(\\)].*//g' \
         -e 's/^http[s]\?:\/\///g' \
-        -e 's/[ ,\(?#\/].*$//g' 
+        -e 's/[ ,\(?#\/].*$//g'
 }
 
 # Return HTTP status code, Excluded (ftp://, \\) or Unavailable (timeout, error)
@@ -57,7 +64,7 @@ INDEX_OUT=$(header_position ${HEADER_OUT})
 #
 head -1 "${INPUT_CSV}" | tee "${SAVE_OUT}"
 sed 1d "${INPUT_CSV}" | while read -r line; do
-    IFS=, c=($line)
+    c=($line)
     RESULT=$(curl_runner ${c[${INDEX_IN}-1]})
     echo "${line}" | awk -F',' -vOFS=',' '{$'"${INDEX_OUT}"'="'"${RESULT}"'"; print}' | tee -a "${SAVE_OUT}"
 done
